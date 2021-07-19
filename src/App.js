117da20bf45cf08d19a9c321d8f668a88f194f48
Nivethithaa M <nivethithaaathithan75@gmail.com>
2021-07-19 19:21:52 +0530

    Counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import React from 'react';
-import Button  from './Button';
+import React,{useState} from 'react';
 import './style.css';
 const First =()=>{
+    const [score, setScore] = useState(0);
+    const [msg,setMsg]= useState("");
+    function handleIncrement() {
+        if(score===25){
+            setMsg("Score should be larger than 25")
+            return
+        }
+        setScore(score+1)
+        setMsg('')
+    }
+    function handleDecrement() {
+        if(score===0){
+            setMsg("Score Shouldn't be less than 0")
+            return
+        }
+        setScore(score-1)
+        setMsg('')
+    }
     return (
         <div>
             <h1 style={{textAlign:'center',color:'black',paddingTop:'10px',textTransform:'uppercase'}}>
-                Lets Upgrade Zero to Hero React js Program <br/>Day1 Assignment
+                Lets Upgrade Zero to Hero React js Program <br/>Day2 Assignment
             </h1>
-            <h2 style={{textAlign:'center',color:'orangered'}}>
-                Hello World!!!
-            </h2>
             <p style={{textAlign:'center',color:'orange',fontSize:'30px'}}>
-              Other Programs Available in LetsUpgrade 
+              Counter
             </p>
             <div style={{display: "flex",justifyContent: "center",alignItems: "center"}}>
-              <Button Course="Data Science" />
-              <Button Course="Machine Learning" />
-              <Button Course="Django" />
-              <Button Course="Web Development" />
+            <button class="btn" onClick={handleIncrement}>Increment</button>
+            <button class="btn" onClick={handleDecrement}>Decrement</button>
+            <button class="btn" onClick={() => setScore(0)} >Reset</button>
+            </div>
+            <div>
+            <h3 style={{textAlign:'center',color:'black',fontFamily:'Lucida Console'}}>The value of score is {score}</h3>
+            <h3 style={{textAlign:'center',color:'crimson',fontFamily:'Lucida Console'}}>{msg}</h3>
             </div>
         </div>
 
--- src/Button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './style.css';
-const Button = ({Course}) =>{
-    return(
-        <button class="btn">{Course}</button>
-    )
-}
-
-export default Button;
